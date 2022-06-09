3beb98b925cacd465a911a1db36abdd25caf985f
Dev Sweeteez <itech@sweeteez.com.br>
2022-06-09 12:31:27 -0300

    Escape

--- src/server.js
+++ src/server.js
@@ -1,11 +1,169 @@
-const express = require("express");
+import express from 'express'
+import SpotifyWebApi from 'spotify-web-api-node';
+import { writeFileSync } from 'fs';
+
+const scopes = [
+  'ugc-image-upload',
+  'user-read-playback-state',
+  'user-modify-playback-state',
+  'user-read-currently-playing',
+  'streaming',
+  'app-remote-control',
+  'user-read-email',
+  'user-read-private',
+  'playlist-read-collaborative',
+  'playlist-modify-public',
+  'playlist-read-private',
+  'playlist-modify-private',
+  'user-library-modify',
+  'user-library-read',
+  'user-top-read',
+  'user-read-playback-position',
+  'user-read-recently-played',
+  'user-follow-read',
+  'user-follow-modify'
+];
+
 const app = express();
-const port = 3000;
+const port = 8888;
+
+// credentials are optional
+const spotifyApi = new SpotifyWebApi({
+  redirectUri: `http://localhost:${port}/callback`,
+  clientId: 'ebc85e6bad48446a9bf963fe12b280a5',
+  clientSecret: '9a6e9935381b4735b0aa14179202e22f'
+});
+
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+  res.send("Hello World")
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.get('/login', (req, res) => {
+  res.redirect(spotifyApi.createAuthorizeURL(scopes));
 });
+
+app.get('/callback', (req, res) => {
+  const error = req.query.error;
+  const code = req.query.code;
+
+  if (error) {
+    console.error('Callback Error:', error);
+    res.send(`Callback Error: ${error}`);
+    return;
+  }
+
+  spotifyApi
+    .authorizationCodeGrant(code)
+    .then(data => {
+      const access_token = data.body['access_token'];
+      const refresh_token = data.body['refresh_token'];
+      const expires_in = data.body['expires_in'];
+      
+
+      spotifyApi.setAccessToken(access_token);
+      spotifyApi.setRefreshToken(refresh_token);
+
+      console.log('access_token:', access_token);
+      console.log('refresh_token:', refresh_token);
+
+      function getMyData() {
+        (async () => {
+          const me = await spotifyApi.getMe();
+          console.log(me.body);
+          getUserPlaylists(me.body.id);
+        })().catch(e => {
+          console.error(e);
+        });
+      }
+
+      async function getUserPlaylists(userName) {
+        const data = await spotifyApi.getUserPlaylists(userName)
+      
+        console.log("---------------+++++++++++++++++++++++++")
+        let playlists = []
+      
+        for (let playlist of data.body.items) {
+          console.log(playlist.name + " " + playlist.id)
+          
+          let tracks = await getPlaylistTracks(playlist.id, playlist.name);
+          console.log(tracks);
+      
+          const tracksJSON = { tracks }
+          let data = JSON.stringify(tracksJSON);
+          writeFileSync(playlist.name+'.json', data);
+        }
+      }
+      
+      //GET SONGS FROM PLAYLIST
+      async function getPlaylistTracks(playlistId, playlistName) {
+      
+        const data = await spotifyApi.getPlaylistTracks(playlistId, {
+          offset: 20,
+          limit: 10,
+          fields: 'items'
+        })
+      
+        console.log('The playlist contains these tracks', data.body);
+        console.log('The playlist contains these tracks: ', data.body.items[0].track);
+        console.log("'" + playlistName + "'" + ' contains these tracks:');
+        let tracks = [];
+      
+        for (let track_obj of data.body.items) {
+          const track = track_obj.track
+          tracks.push(track);
+          console.log(track.name + " : " + track.artists[0].name)
+        }
+        
+        console.log("---------------+++++++++++++++++++++++++")
+        return tracks;
+      }
+
+      console.log(
+        `Sucessfully retreived access token. Expires in ${expires_in} s.`
+      );
+      getMyData()
+      res.send(
+        
+      );
+
+      setInterval(async () => {
+        const data = await spotifyApi.refreshAccessToken();
+        const access_token = data.body['access_token'];
+
+        console.log('The access token has been refreshed!');
+        console.log('access_token:', access_token);
+        
+        spotifyApi.setAccessToken(access_token);
+        
+        
+      }, expires_in / 2 * 1000);
+    })
+    .catch(error => {
+      console.error('Error getting Tokens:', error);
+      res.send(`Error getting Tokens: ${error}`);
+    });
+})  
+
+app.listen(port, () =>
+  console.log(
+    `Server is running at ${port}, now go to /login`
+  )
+);
+
+
+
+/*
+// Get Elvis' albums
+spotifyApi.getArtistAlbums(
+    '43ZHCT0cAZBISjO8DG9PnE',
+    { limit: 10, offset: 20 },
+    function(err, data) {
+      if (err) {
+        console.error('Something went wrong!');
+      } else {
+        console.log(data.body);
+      }
+    }
+  )
+*/
